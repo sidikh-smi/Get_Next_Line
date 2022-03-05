7b102a550d2be0bb8cb252f3d67bfbd9ef103eec
Sidi Khaliffa <skhaliff@e1r12p1.1337.ma>
2022-03-05 15:05:18 +0100

    push

---
+++ get_next_line.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skhaliff <skhaliff@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/12 21:14:34 by skhaliff          #+#    #+#             */
+/*   Updated: 2021/12/16 23:59:23 by skhaliff         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include<unistd.h>
+# include<stdio.h>
+# include<stdlib.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+size_t	ft_strlen(char *s);
+char	*ft_line(char *str);
+char	*new_line(char *str);
+char	*ft_strdup(char *s1);
+
+#endif
---
+++ get_next_line_bonus.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: skhaliff <skhaliff@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/16 23:57:16 by skhaliff          #+#    #+#             */
+/*   Updated: 2021/12/18 19:13:29 by skhaliff         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+# include<unistd.h>
+# include<stdio.h>
+# include<stdlib.h>
+# include<limits.h>
+
+char	*get_next_line(int fd);
+char	*ft_strjoin(char *s1, char *s2);
+char	*ft_strchr(char *s, int c);
+size_t	ft_strlen(char *s);
+char	*ft_line(char *str);
+char	*new_line(char *str);
+char	*ft_strdup(char *s1);
+
+#endif
